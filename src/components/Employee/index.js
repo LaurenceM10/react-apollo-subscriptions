7263879115dc6f6cab9ca06a584102c93561d1d1
LaurenceM10 <laurenmontenegro10@gmail.com>
2019-04-16 14:22:26 -0600

    Add: employee component to list employees and subscibe to Apollo Server pubs

---
+++ src/components/Employee/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {withApollo} from "react-apollo";
+
+class Employee extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default withApollo(Employee);
