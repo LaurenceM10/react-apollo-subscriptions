2522a797897406eb3548d1925c5b0256afac9e5d
LaurenceM10 <laurenmontenegro10@gmail.com>
2019-04-16 13:25:22 -0600

    Add: url in .gitignore files

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,69 @@
 import React, {Component} from 'react';
-import logo from '../logo.svg';
 import './App.css';
-import ApolloClient from "apollo-boost";
+import {ApolloProvider} from "react-apollo";
+import ApolloClient from "apollo-client";
+import {WebSocketLink} from 'apollo-link-ws';
+import {HttpLink} from 'apollo-link-http';
+import {split} from 'apollo-link';
+import {getMainDefinition} from 'apollo-utilities';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {setContext} from "apollo-link-context";
+import { URL } from "../../config/urls";
+
+const AUTH_TOKEN = localStorage.getItem('token');
+
+const httpLink = new HttpLink({
+    uri: URL,
+});
+
+const authLink = setContext((_, {headers}) => {
+    // get the authentication token from local storage if it exists
+    const token = AUTH_TOKEN;
+    // return the headers to the context so httpLink can read them
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : "",
+        }
+    }
+});
+
+const wsLink = new WebSocketLink({
+    uri: `ws://localhost:4000`,
+    options: {
+        reconnect: true,
+        connectionParams: {
+            authToken: localStorage.getItem(AUTH_TOKEN),
+        }
+    }
+});
+
+const link = split(
+    // split based on operation type
+    ({query}) => {
+        const {kind, operation} = getMainDefinition(query);
+        return kind === 'OperationDefinition' && operation === 'subscription';
+    },
+    wsLink,
+    authLink.concat(httpLink),
+);
+
 
 const client = new ApolloClient({
-    uri: "http://localhost:4000"
+    link: link,
+    cache: new InMemoryCache()
 });
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
+            <ApolloProvider client={client}>
+                <code>
+                    <h1>
+                        <center>Hello World!</center>
+                    </h1>
+                </code>
+            </ApolloProvider>
         );
     }
 }
